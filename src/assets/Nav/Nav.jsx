74bbd9b6498aac6835be270a40b9913677232fd8
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-30 09:37:27 +0530

    first commit

---
+++ src/assets/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+
+const Nav = () => {
+      return (
+            <nav>
+                  <div>
+                        <h1>Hand book</h1>
+                  </div>
+            </nav>
+      );
+};
+
+export default Nav;
